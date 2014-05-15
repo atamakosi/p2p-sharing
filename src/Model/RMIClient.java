23da5f8e4741746fada36c275ad96b452e9ebf97
Adam Mcnabb <adamrmcnabb@gmail.com>
2014-05-15 15:34:32 +1200

    RMIClient methods added to initialize registry

--- src/Model/RMIClient.java
+++ src/Model/RMIClient.java
@@ -26,24 +26,24 @@ public class RMIClient implements RMIInterface {
         
     }
     
-    public static void main(String[] args)  {
-        RMIClient client = new RMIClient();
+    public void start() {
         try {
-            RMIInterface stub = (RMIInterface) UnicastRemoteObject.exportObject(client, 0);
-            Registry registry = LocateRegistry.getRegistry(REGISTRY_URL);
-            registry.rebind("stub", stub);
-            System.out.println("stubs in registry");
-            try {
-                String[] bindings = Naming.list(REGISTRY_URL);
-                
-            }   catch (MalformedURLException e)     {
-                System.out.println("error retrieving names " + e);
+        RMIInterface stub = (RMIInterface) UnicastRemoteObject.exportObject(this, 0);
+        Registry registry = LocateRegistry.getRegistry(REGISTRY_URL);
+        registry.rebind("stub", stub);
+        System.out.println("stubs in registry");
+        try {
+            String[] bindings = Naming.list(REGISTRY_URL);
+            for (String str : bindings )    {
+                System.out.println(str);
             }
+        }   catch (MalformedURLException e)     {
+            System.out.println("error retrieving names " + e);
+        }
         } catch (RemoteException ex) {
             Logger.getLogger(RMIClient.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
 
     @Override
     public File getFile() throws RemoteException {
